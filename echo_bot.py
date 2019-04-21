ea201fe2f97a73bb0ea60eb3df54a727b576ac2d
theflair <49837777+theflair@users.noreply.github.com>
2019-04-21 22:34:57 +0530

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -5,7 +5,7 @@
 
 import telebot
           
-API_TOKEN = 'Your_API_Token_Here'
+API_TOKEN = '640628709:AAE1FjeMUJj_mEjeH_3GrnnssK6M7XDoQrg'
           
 bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
@@ -14,28 +14,28 @@ PORT = int(os.environ.get('PORT', '8443'))
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-bot.reply_to(message, "\nHi there, I am EchoBot.\nI am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\n")
+          bot.reply_to(message, "\nHi there, I am EchoBot.\nI am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\n")
           
           
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-bot.reply_to(message, message.text)
+          bot.reply_to(message, message.text)
 
 
 @server.route('/' + API_TOKEN, methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+          bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+          return "!", 200
 
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='Your_App_Name_Link_Here' + API_TOKEN)
-    return "!", 200
+          bot.remove_webhook()
+          bot.set_webhook(url='https://theabcd.herokuapp.com/' + API_TOKEN)
+          return "!", 200
 
 
 if __name__ == "__main__":
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))    
+          server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))    
     
